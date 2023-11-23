8be4ad1864dd23e063e5e9434bdf539319710b1b
MAHESHPOLISETTY <138252706+MAHESHPOLISETTY@users.noreply.github.com>
2023-11-23 09:05:11 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -5,14 +5,13 @@ sudo systemctl start httpd
 sudo systemctl enable httpd
 sudo systemctl status httpd >> /home/ec2-user/httpdstatus.txt
 sudo yum -y install python3 >> /home/ec2-user/pythonstatus.txt
-
+sudo yum -y install python3-pip >> /home/ec2-user/pip3status1.txt
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade -y
 sudo yum -y install java-17*
 sudo yum install jenkins -y
-
 sudo systemctl start jenkins
 sudo systemctl enable jenkins
 sudo systemctl status jenkins >> /home/ec2-user/jnknsstatus.txt
