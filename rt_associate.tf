a28b82156a100e8d9af583ae698071735c366a71
MAHESHPOLISETTY <138252706+MAHESHPOLISETTY@users.noreply.github.com>
2023-11-25 17:55:28 +0530

    Update rt_associate.tf

--- rt_associate.tf
+++ rt_associate.tf
@@ -7,3 +7,11 @@ resource "aws_route_table_association" "prj3_mahi_pub_rt_ass" {
   route_table_id = aws_route_table.prj3_mahi_pub_rt.id
 
 }
+
+resource "aws_route_table_association" "prj3_mahi_pub2_rt_ass" {
+
+  subnet_id = aws_subnet.prj3_mahi_pub2_sub.id
+
+  route_table_id = aws_route_table.prj3_mahi_pub_rt.id
+
+}
