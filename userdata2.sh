32d93d6a6ed1e249ca31c7fdac1288dd64cadff2
MAHESHPOLISETTY <138252706+MAHESHPOLISETTY@users.noreply.github.com>
2023-11-25 09:13:20 +0530

    Update userdata2.sh

--- userdata2.sh
+++ userdata2.sh
@@ -5,5 +5,5 @@ sudo systemctl start docker
 sudo systemctl enable docker
 sudo systemctl status docker >> /home/ec2-user/dckrstatus.txt
 sudo chmod 666 /var/run/docker.sock
-sudo rpm -ivh https://github.com/aquasecurity/trivy/releases/download/v0.18.3/trivy_0.18.3_Linux-64bit.rpm ###u can keep this or u can ignore this step as anyway we are scanning it in master before pushing it to docker 
+sudo rpm -ivh https://github.com/aquasecurity/trivy/releases/download/v0.18.3/trivy_0.18.3_Linux-64bit.rpm   ###u can keep this or u can ignore this step as anyway we are scanning it in master before pushing it to docker 
 sudo yum update -y 
