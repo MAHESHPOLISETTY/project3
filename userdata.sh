2ab4745ddc27527a2e56937df253c12eb0c9f1a3
MAHESHPOLISETTY <138252706+MAHESHPOLISETTY@users.noreply.github.com>
2023-11-25 18:32:48 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -18,6 +18,7 @@ sudo systemctl start docker
 sudo systemctl enable docker
 sudo systemctl status docker >> /home/ec2-user/dckrstatus.txt
 sudo chmod 666 /var/run/docker.sock
+sudo chmod 666 /var/run/docker.sock
 sudo rpm -ivh https://github.com/aquasecurity/trivy/releases/download/v0.18.3/trivy_0.18.3_Linux-64bit.rpm ##for installing trivy
 sudo yum update -y 
 
